40719993fd4d6bfefc98f20c6cbbc5a047cc3283
Gowtham Venkatesh <RB00230@rheinbrucke.loc>
2019-06-14 18:01:38 +0530

    End Of Section 12

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -34,7 +34,7 @@ namespace DatingApp.API.Controllers
             return Ok(userstoreturn);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name= "GetUser")]
 
         public async Task<IActionResult> GetUser(int id)
         {
